736e2932647525d2afe0a9f02641453ba596e9d9
Priyanka_Bed <108406975+PriyankaBed@users.noreply.github.com>
2022-10-26 15:25:22 +0200

    code changed
    code changed

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
-require("dotenv/config")
-require("./db")
+require("dotenv/config");
+require("./db");
 const cors = require("cors");
-const express = require('express')
+var cookieParser = require("cookie-parser");
+const express = require("express");
 const { videoRouter } = require("./routes/video");
 const { authRouter } = require("./routes/auth");
 const { userRouter } = require("./routes/user");
 const { errorHandler } = require("./middlewares/errorHandler");
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 4000;
 
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        credentials: true,
+    })
+);
+
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:3000"]
-}));
+
 app.use("/videos", videoRouter);
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
